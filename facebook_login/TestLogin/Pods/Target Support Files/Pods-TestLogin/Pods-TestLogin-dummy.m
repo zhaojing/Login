588c603d33f165cbd91ef895d6d2ce03e33189f7
wwx391909 <wanglvju@huawei.com>
2016-08-17 10:29:38 +0800

    deleted unneeded facebook

--- facebook_login/TestLogin/Pods/Target Support Files/Pods-TestLogin/Pods-TestLogin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestLogin : NSObject
-@end
-@implementation PodsDummy_Pods_TestLogin
-@end
--- facebook_login/TestLogin/TestLogin/FB_ObjectiveC_Bridging.h
+++
@@ -1,3 +0,0 @@
-#import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-#import <FBSDKShareKit/FBSDKShareKit.h>
